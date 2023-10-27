025cb8d1ccbabb826b1fb7617fd753ea4534688b
Kimberly <kimberlysed07@gmail.com>
2023-10-27 00:04:15 -0500

    Resolviendo error de swtich

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 var changeLanguage = document.querySelector(".check");
 changeLanguage.addEventListener("click", language);
 
-function language(){
-    let id=changeLanguage.checked;
-    if(id==true){
-        location.href="/index.html";
-    }else{
-        location.href="en/index.html";
-        
+function language() {
+    let id = changeLanguage.checked;
+    if (id == true) {
+        location.href = "index.html"; // Ruta relativa a la página principal
+    } else {
+        location.href = "en/index.html"; // Ruta relativa a la página en inglés
     }
 }
