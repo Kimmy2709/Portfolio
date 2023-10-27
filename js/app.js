d3e3b20c7e335c7a9b68403451d0decb916d2858
Kimberly <kimberlysed07@gmail.com>
2023-10-27 00:09:50 -0500

    Modificando error en switch part II

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 var changeLanguage = document.querySelector(".check");
 changeLanguage.addEventListener("click", language);
 
+// Al cargar la página, verifica si el usuario ha seleccionado un idioma previamente
+if (localStorage.getItem("selectedLanguage") === "en") {
+    changeLanguage.checked = true;
+}
+
 function language() {
     let id = changeLanguage.checked;
-    if (id == true) {
+    if (id === true) {
+        // Usuario seleccionó cambiar a idioma predeterminado
+        localStorage.setItem("selectedLanguage", "default");
         location.href = "index.html"; // Ruta relativa a la página principal
     } else {
+        // Usuario seleccionó cambiar al idioma en inglés
+        localStorage.setItem("selectedLanguage", "en");
         location.href = "en/index.html"; // Ruta relativa a la página en inglés
     }
 }
