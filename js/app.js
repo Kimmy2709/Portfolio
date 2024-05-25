2ffdb931fd773155dde14c999f2531e2d9926658
Kimberly <kimberlysed07@gmail.com>
2024-05-25 18:50:57 -0500

    fixing lang switch button

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
 var changeLanguage = document.querySelector(".check");
-changeLanguage.addEventListener("click", language);
 
-// Al cargar la página, verifica si el usuario ha seleccionado un idioma previamente
-if (localStorage.getItem("selectedLanguage") === "en") {
-    changeLanguage.checked = true;
-}
+        if (localStorage.getItem("selectedLanguage") === "en") {
+            changeLanguage.checked = true;
+            if (window.location.pathname !== "/en/index.html") {
+                window.location.href = "en/index.html";
+            }
+        } else {
+            changeLanguage.checked = false;
+            if (window.location.pathname !== "/index.html") {
+                window.location.href = "index.html";
+            }
+        }
 
-function language() {
-    let id = changeLanguage.checked;
-    if (id === true) {
-        // Usuario seleccionó cambiar a idioma predeterminado
-        localStorage.setItem("selectedLanguage", "default");
-        location.href = "index.html"; // Ruta relativa a la página principal
-    } else {
-        // Usuario seleccionó cambiar al idioma en inglés
-        localStorage.setItem("selectedLanguage", "en");
-        location.href = "en/index.html"; // Ruta relativa a la página en inglés
-    }
-}
+        changeLanguage.addEventListener("click", language);
+
+        function language() {
+            if (changeLanguage.checked) {
+                localStorage.setItem("selectedLanguage", "en");
+                window.location.href = "en/index.html";
+            } else {
+                localStorage.setItem("selectedLanguage", "default");
+                window.location.href = "index.html";
+            }
+        }
