fb7d9a8ca07147877314362f7ce69dd8437e2662
Kimberly <kimberlysed07@gmail.com>
2024-05-25 19:02:40 -0500

    test2

--- js/app.js
+++ js/app.js
@@ -1,25 +1,19 @@
 var changeLanguage = document.querySelector(".check");
+changeLanguage.addEventListener("click", language);
 
-        if (localStorage.getItem("selectedLanguage") === "en") {
-            changeLanguage.checked = true;
-            if (window.location.pathname !== "/en-index.html") {
-                window.location.href = "en-index.html";
-            }
-        } else {
-            changeLanguage.checked = false;
-            if (window.location.pathname !== "/index.html") {
-                window.location.href = "index.html";
-            }
-        }
+if (localStorage.getItem("selectedLanguage") === "en-index") {
+    changeLanguage.checked = true;
+}
 
-        changeLanguage.addEventListener("click", language);
-
-        function language() {
-            if (changeLanguage.checked) {
-                localStorage.setItem("selectedLanguage", "en");
-                window.location.href = "en-index.html";
-            } else {
-                localStorage.setItem("selectedLanguage", "default");
-                window.location.href = "index.html";
-            }
-        }
+function language() {
+    let id = changeLanguage.checked;
+    if (id === true) {
+        
+        localStorage.setItem("selectedLanguage", "default");
+        location.href = "index.html"; 
+    } else {
+       
+        localStorage.setItem("selectedLanguage", "en");
+        location.href = "en-index.html"; 
+    }
+}
