e390e48aed173636d0e74876c5600447ef397784
Kimberly <kimberlysed07@gmail.com>
2024-05-25 19:31:15 -0500

    test3

--- js/app.js
+++ js/app.js
@@ -1,19 +1,22 @@
 var changeLanguage = document.querySelector(".check");
 changeLanguage.addEventListener("click", language);
 
-if (localStorage.getItem("selectedLanguage") === "en-index") {
+if (localStorage.getItem("selectedLanguage") === "en") {
     changeLanguage.checked = true;
 }
 
-function language() {
-    let id = changeLanguage.checked;
-    if (id === true) {
+changeLanguage.addEventListener("change", language);
+    
+    function language() {
         
-        localStorage.setItem("selectedLanguage", "default");
-        location.href = "index.html"; 
-    } else {
-       
-        localStorage.setItem("selectedLanguage", "en");
-        location.href = "en-index.html"; 
+        var isChecked = changeLanguage.checked;
+        
+        
+        if (isChecked) {
+            localStorage.setItem("selectedLanguage", "default");
+            location.href = "index.html"; 
+        } else {
+            localStorage.setItem("selectedLanguage", "en");
+            location.href = "en-index.html"; 
     }
 }
